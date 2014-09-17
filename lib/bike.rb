4247d3a7030beb85f41da2e6ec9674d2151d75f5
Tim Scully <Scully87.ts@googlemail.com>
2014-09-17 12:46:32 +0100

    commiting instance variables

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,19 @@
 class Bike
 
-	def broken?
+	def initialize
+		@broken = false
+		#the @ gives us an instance variable meaning
+		# we can use 'broken' in all other methods where
+		#each method can update them sepeartely
 	end
+
+	def broken?
+		@broken
+	end
+
+	def break!
+		@broken = true 
+	end
+
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,8 +4,14 @@ require 'bike'
 describe Bike do
 	
 	it "should not be broken wehen we create it" do
-		bike = Bike.new
+		bike = Bike.new # initialise a new object of bike class
 		expect(bike).not_to be_broken
+	end
+
+	it "should be able to break" do
+		bike = Bike.new
+		bike.break!
+		expect(bike).to be_broken
 	end
 
 end
