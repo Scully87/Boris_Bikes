6c3c377538d4511ad2a9e0eece28c291880efcd5
Scully87 <scully87.ts@googlemail.com>
2014-09-24 18:59:43 +0100

    bozza

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -7,7 +7,7 @@ describe BikeContainer do
 	let(:bike) { Bike.new }
 	let(:holder) { ContainerHolder.new }
 
-	it "should accept a bike" do
+	it "should accept a bike" do 
 		expect(holder.bike_count).to eq(0)
 		holder.dock(bike)
 		expect(holder.bike_count).to eq(1)
